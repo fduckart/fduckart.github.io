839a3782c7b5c5c7581126402a444e6bf4aefff7
Frank Duckart <duckart@computer.org>
2017-08-06 16:28:03 -1000

    Add favorite songs list.

--- js/fave-app.js
+++ js/fave-app.js
@@ -9,30 +9,31 @@ var faveApp = angular.module('faveApp', []);
             {title: 'Another Green World', artist: 'Brian Eno'},
             {title: 'Kind Of Blue', artist: 'Miles Davis'},
             {title: 'Moondance', artist: 'Van Morrison'},
+            {title: 'Highway to Hell', artist: 'AC/DC'},
             {title: 'Horses', artist: 'Patti Smith'},
             {title: 'How Will the Wolf Survive?', artist: 'Los Lobos'},
             {title: 'El Rayo-X', artist: 'David Lindley'},
-            {title: 'Rid Of Me', artist: 'PJ Harvey'},
+            {title: 'Rid of Me', artist: 'PJ Harvey'},
             {title: 'What\'s Going On?', artist: 'Marvin Gaye'},
-            {title: 'Purple Rain', artist: 'Prince'},
-            {title: 'Highway to Hell', artist: 'AC/DC'}
+            {title: 'Purple Rain', artist: 'Prince'}
         ];
     }
     faveApp.controller("RecordJsController", RecordJsController);
 
     function FilmJsController($scope) {
         $scope.films = [
-            {title: 'Citizen Kane', director: 'Orson Welles'},
-            {title: 'Chinatown', director: 'Brian DePalma'},
-            {title: 'Blade Runner', director: 'Ridely Scott'},
-            {title: 'Vertigo', director: 'Alfred Hitchcock'},
-            {title: 'Sunset Boulevard', director: 'Billy Wilder'},
-            {title: 'The Rules of the Game', director: 'Jean Renoir'},
-            {title: 'Annie Hall', director: 'Woody Allen'},
-            {title: 'The Wizard of Oz', director: 'Victor Fleming'},
-            {title: 'Out of the Past', director: 'Jacques Tourneur'},
-            {title: 'Casablanca', director: 'Michael Curtiz'},
-            {title: 'The Conversation', director: 'Francis Ford Coppola'}
+            {title: 'Citizen Kane', director: 'Orson Welles', year: 1900},
+            {title: 'Chinatown', director: 'Brian DePalma', year: 1900},
+            {title: 'Blade Runner', director: 'Ridely Scott', year: 1900},
+            {title: 'Vertigo', director: 'Alfred Hitchcock', year: 1900},
+            {title: 'Sunset Boulevard', director: 'Billy Wilder', year: 1900},
+            {title: 'The Rules of the Game', director: 'Jean Renoir', year: 1900},
+            {title: 'Annie Hall', director: 'Woody Allen', year: 1900},
+            {title: 'The Wizard of Oz', director: 'Victor Fleming', year: 1900},
+            {title: 'Out of the Past', director: 'Jacques Tourneur', year: 1900},
+            {title: 'Casablanca', director: 'Michael Curtiz', year: 1900},
+            {title: 'Ex Machina', director: 'Alex Garland', year: 1900},
+            {title: 'The Conversation', director: 'Francis Ford Coppola', year: 1900}
         ];
     }
     faveApp.controller("FilmJsController", FilmJsController);
@@ -52,5 +53,25 @@ var faveApp = angular.module('faveApp', []);
         ];
     }
     faveApp.controller("BookJsController", BookJsController);
+
+    function SongJsController($scope) {
+        $scope.songs = [
+            {title: 'Sheela-Na-Gig', artist: 'PJ Harvey'},
+            {title: 'Sky Saw', artist: 'Brian Eno'},
+            {title: 'Into The Mystic', artist: 'Van Morrison'},
+            {title: 'Do It Again', artist: 'Steely Dan'},
+            {title: 'Hate It Here', artist: 'Wilco'},
+            {title: 'Gimme Shelter', artist: 'The Rolling Stones'},
+            {title: 'Fast Car', artist: 'Tracy Chapman'},
+            {title: 'Planet Claire', artist: 'The B-52s'},
+            {title: 'Watching the Detectives', artist: 'Elvis Costello'},
+            {title: 'Money', artist: 'Pink Floyd'},
+            {title: 'Sweet Home Alabama', artist: 'Lynyrd Skynyrd'},
+            {title: 'Blitzkrieg Bop', artist: 'Ramones'},
+            {title: 'Heroes', artist: 'David Bowie'},
+            {title: 'Guilty', artist: 'Randy Newman'}
+        ];
+    }
+    faveApp.controller("SongJsController", SongJsController);
 
 })();
