b5c4ffbae517b044176a087327cfb5e4565f1b50
Frank Duckart <duckart@computer.org>
2022-12-04 16:44:02 -1000

    Read from external JSON files.

--- js/fave-app.js
+++ js/fave-app.js
@@ -1,85 +1,50 @@
 'use strict';
-var faveApp = angular.module('faveApp', []);
+const faveApp = angular.module('faveApp', []);
+
+faveApp.constant('App', {
+    Url: {
+        DATA_BOOKS: 'https://raw.githubusercontent.com/fduckart/fduckart.github.io/master/files/books.json',
+        DATA_FILMS: 'https://raw.githubusercontent.com/fduckart/fduckart.github.io/master/files/films.json',
+        DATA_RECORDS: 'https://raw.githubusercontent.com/fduckart/fduckart.github.io/master/files/records.json',
+        DATA_SONGS: 'https://raw.githubusercontent.com/fduckart/fduckart.github.io/master/files/songs.json'
+    }
+});
 
 (function() {
 
-    function RecordJsController($scope, $http) {
+    function RecordJsController($scope, $http, App) {
         $scope.init = () => {
-            $scope.records = [
-                {title: 'Shoot Out The Lights', artist: 'Richard & Linda Thompson'},
-                {title: 'Another Green World', artist: 'Brian Eno'},
-                {title: 'Kind Of Blue', artist: 'Miles Davis'},
-                {title: 'Rumours', artist: 'Fleetwood Mac'},
-                {title: 'Moondance', artist: 'Van Morrison'},
-                {title: 'If You Want Blood You\'ve Got It', artist: 'AC/DC'},
-                {title: 'Horses', artist: 'Patti Smith'},
-                {title: 'How Will the Wolf Survive?', artist: 'Los Lobos'},
-                {title: 'El Rayo-X', artist: 'David Lindley'},
-                {title: 'Rid of Me', artist: 'PJ Harvey'},
-                {title: 'Remain in Light', artist: 'Talking Heads'},
-                {title: 'What\'s Going On', artist: 'Marvin Gaye'},
-                {title: 'Shotgun Willie', artist: 'Willie Nelson'},
-                {title: 'Purple Rain', artist: 'Prince'}
-            ];
+            $http.get(App.Url.DATA_RECORDS).then((response) => {
+                $scope.records = response.data.records;
+            });
         };
     }
     faveApp.controller("RecordJsController", RecordJsController);
 
-    function FilmJsController($scope) {
-        $scope.films = [
-            {title: 'Citizen Kane', director: 'Orson Welles', year: 1941},
-            {title: 'Chinatown', director: 'Roman Polanski', year: 1974},
-            {title: 'Blade Runner', director: 'Ridely Scott', year: 1982},
-            {title: 'Vertigo', director: 'Alfred Hitchcock', year: 1958},
-            {title: 'Wings of Desire', director: 'Wim Wenders', year: 1987},
-            {title: 'Sunset Boulevard', director: 'Billy Wilder', year: 1950},
-            {title: 'The Rules of the Game', director: 'Jean Renoir', year: 1939},
-            {title: 'In the Mood for Love', director: ' Kar Wai Wong', year: 2000},
-            {title: 'Annie Hall', director: 'Woody Allen', year: 1977},
-            {title: 'The Wizard of Oz', director: 'Victor Fleming', year: 1939},
-            {title: 'Out of the Past', director: 'Jacques Tourneur', year: 1947},
-            {title: 'Casablanca', director: 'Michael Curtiz', year: 1942},
-            {title: 'Ex Machina', director: 'Alex Garland', year: 2014},
-            {title: 'The Conversation', director: 'Francis Ford Coppola', year: 1974}
-        ];
+    function FilmJsController($scope, $http, App) {
+        $scope.init = () => {
+            $http.get(App.Url.DATA_FILMS).then((response) => {
+                $scope.films = response.data.films;
+            });
+        };
     }
     faveApp.controller("FilmJsController", FilmJsController);
 
-    function BookJsController($scope) {
-        $scope.books = [
-            {title: 'Lolita', author: 'Vladimir Nabokov'},
-            {title: 'Nineteen Eighty Four', author: 'George Orwell'},
-            {title: 'Lucky Jim', author: 'Kingsley Amis'},
-            {title: 'Fear and Loathing in Las Vegas', author: 'Hunter S. Thomspon'},
-            {title: 'Things Fall Apart', author: 'Chinua Achebe'},
-            {title: 'Brave New World', author: 'Aldous Huxley'},
-            {title: 'Frankenstein', author: 'Mary Shelley'},
-            {title: 'Portnoy\'s Complaint', author: 'Philip Roth'},
-            {title: 'Experience', author: 'Martin Amis'},
-            {title: 'Life', author: 'Keith Richards'}
-        ];
+    function BookJsController($scope, $http, App) {
+        $scope.init = () => {
+            $http.get(App.Url.DATA_BOOKS).then((response) => {
+                $scope.books = response.data.books;
+            });
+        };
     }
     faveApp.controller("BookJsController", BookJsController);
 
-    function SongJsController($scope) {
-        $scope.songs = [
-            {title: 'Sheela-Na-Gig', artist: 'PJ Harvey'},
-            {title: 'Sky Saw', artist: 'Brian Eno'},
-            {title: 'Into The Mystic', artist: 'Van Morrison'},
-            {title: 'Do It Again', artist: 'Steely Dan'},
-            {title: 'Sweet Home Alabama', artist: 'Lynyrd Skynyrd'},
-            {title: 'Hate It Here', artist: 'Wilco'},
-            {title: 'Gimme Shelter', artist: 'The Rolling Stones'},
-            {title: 'Sultans of Swing', artist: 'Dire Straits'},
-            {title: 'Fast Car', artist: 'Tracy Chapman'},
-            {title: 'Chameleon', artist: 'Herbie Hancock'},
-            {title: 'Planet Claire', artist: 'The B-52s'},
-            {title: 'Watching the Detectives', artist: 'Elvis Costello'},
-            {title: 'Money', artist: 'Pink Floyd'},
-            {title: 'Blitzkrieg Bop', artist: 'Ramones'},
-            {title: 'Heroes', artist: 'David Bowie'},
-            {title: 'Guilty', artist: 'Randy Newman'}
-        ];
+    function SongJsController($scope, $http, App) {
+        $scope.init = () => {
+            $http.get(App.Url.DATA_SONGS).then((response) => {
+                $scope.songs = response.data.songs;
+            });
+        };
     }
     faveApp.controller("SongJsController", SongJsController);
 
