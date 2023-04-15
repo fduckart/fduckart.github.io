d7d8b73ce620bdd5ae9d25eb1764217739a4efec
Frank Duckart <duckart@computer.org>
2023-04-14 15:35:54 -1000

    Change data source.

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const URL_DATA_ROOT = "https://raw.githubusercontent.com/fduckart/fduckart.github.io/data/v2/";
+export const URL_DATA_ROOT = "https://raw.githubusercontent.com/fduckart/fduckart.github.io/data/latest/";
 
